87ec6821903cc422fc53fd4a6ed7f5f81701de48
Tim Bielawa <tbielawa@redhat.com>
2016-07-12 08:35:54 -0400

    Minimize the number of imports in the docs config
    Re: #1

--- docs/conf.py
+++ docs/conf.py
@@ -20,9 +20,6 @@ import os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath(".."))
-sys.path.insert(1, os.path.abspath("../lib_openshift"))
-sys.path.insert(2, os.path.abspath("../lib_openshift/apis"))
-sys.path.insert(3, os.path.abspath("../lib_openshift/models"))
 
 
 # -- General configuration ------------------------------------------------
